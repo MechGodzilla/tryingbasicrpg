a0729841b8a5e70f647efd7cacfd7bca02858c75
MechGodzilla <serpentining@gmail.com>
2017-04-11 17:32:13 -0700

    finished.

--- New folder/mainrun.py
+++ New folder/mainrun.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import random
+import pickle
 
 from enemy import Goblin, Zombie
 from player import Player
@@ -18,7 +19,15 @@ def main():
     if option == "1":
         start()
     elif option == "2":
-        pass
+        if os.path.exists("savefile") == True:
+            with open('savefile', 'rb') as f:
+                global playerIG
+                playerIG = pickle.load(f)
+            print("Loading save...")
+            start1()
+        else:
+            print("File not found")
+            main()
     elif option == "3":
         sys.exit()
     else:
@@ -45,18 +54,64 @@ def start1():
     print ("2.) Store")
     print ("3.) Save")
     print ("4.) Exit")
+    print("5.) Inventory")
     option = input("-> ")
     if option == "1":
         prefight()
     elif option == "2":
         store()
     elif option == "3":
-        pass
+        os.system("cls")
+        with open('savefile', 'wb') as f:
+            pickle.dump(playerIG, f)
+            print("Game has been saved!")
+            start1()
     elif option == "4":
         sys.exit()
+    elif option == "5":
+        inventory()
     else:
         start1()
-#
+
+def inventory():
+    print("What do you want to do?\n"
+          "1.) Equip Weapon\n"
+          "2.) Go back")
+    inv_option = input("-> ")
+    if inv_option == "1":
+        equip()
+    elif inv_option == "2":
+        start1()
+    else:
+        print("Invalid command. Please try again")
+        inventory()
+
+
+def equip():
+    print("What do you want to equip?")
+    for weapon in playerIG.weapon_inventory:
+        print (weapon)
+    print("b to go back")
+    option = input("-> ")
+    if option == playerIG.current_weapon:
+        print("You already have the weapon equipped")
+        option = input(" ")
+        equip()
+    elif option == "b":
+        inventory()
+    elif option in playerIG.weapon_inventory:
+        playerIG.current_weapon = option
+        print("%s has been eqipped." % option)
+        option = input(" ")
+        equip()
+    else:
+        print("%s not found. Plesae try again" % option)
+        equip()
+
+
+
+
+
 # goblin_ig = Goblin("Goblin")
 # zombie_ig = Zombie("Zombie")
 
@@ -155,6 +210,7 @@ def store():
     print("Welcome to the shop!")
     print("What would you like to buy")
     print("1.) Great Sword")
+    print("2.) Potion")
     print("3.) back")
     buying_item = input(' ')
     if buying_item in weapons:
@@ -168,6 +224,11 @@ def store():
             print("Not enough gold.")
             option = input(' ')
             store()
+    elif buying_item == "Potion" or buying_item == "2":
+        playerIG.pots +=1
+        playerIG.gold -= 10
+        print("You bought 1 potion. You have %i" % playerIG.pots)
+        store()
     elif buying_item == "back":
         start1()
     else:
